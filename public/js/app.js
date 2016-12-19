29a04b090964cf48fba7a1ec66a4be8874d1d61d
Joshua Lee <joshualee@iconfigure.org>
2016-12-18 22:15:41 -1000

    updated app.js file, can now access images and display onto html page

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ console.log('my sanity here');
 (function () {
 
   function reqListener() {
-    // console.log(this.responseText)
+    console.log(this.responseText)
 
     let myParser = JSON.parse(this.responseText)
     console.log('parsed info**', myParser)
@@ -14,34 +14,43 @@ console.log('my sanity here');
 
     for(var k = 0; k < parser.length; k++){
 
+      //creates a temporary obj that stores parsed info from reddit
+      tempObj = {
+        title: parser[k].data.title,
+        links: parser[k].data.thumbnail
+      }
+
+
      //creates div and appends to HTML
     let createDiv = document.createElement('div');
     createDiv.id = 'pic-container'; //assigns the ID
+
 
     //calls the element from html and assings addToHtml
     let addToHtml = document.getElementById('animalCage');
     addToHtml.appendChild(createDiv);// appends the div
 
-    //create some children for div
+    //creates div to store picture
+    let picture = document.createElement('div')
+    addToHtml.appendChild(picture);
+
+    //creates children paragraphs to store redit description and appends it to pciture div
     let childElems = document.createElement('p');
     childElems.id = 'child-Elems';
     document.getElementById('animalCage');
-    addToHtml.appendChild(childElems)
+    picture.appendChild(childElems)
 
-      //creates a temporary obj that stores parsed info from reddit
-      tempObj = {
-        title: parser[k].data.title,
-        links: parser[k].data.permalink
-      }
+    let imgsrc = tempObj.links;//assigns tempObj.links to the imgsrc to be inserted into html
+    let link =  imgsrc;
 
-    let imgsrc = tempObj.links;
+    picture.innerHTML = '<img src = "' + link + '">'
 
 
       console.log('sanity', tempObj.title)
 
     createDiv.innerHTML = tempObj.title;
 
-    //   console.log('hey:', tempObj.links)
+      console.log('hey:', link)
     }
 
 
@@ -49,7 +58,7 @@ console.log('my sanity here');
 
   const redditRequest = new XMLHttpRequest();
   redditRequest.addEventListener('load', reqListener);
-  redditRequest.open('GET', 'https://www.reddit.com/r/funnyanimal.json');
+  redditRequest.open('GET', 'https://www.reddit.com/r/cat+pictures.json');
   redditRequest.send();
 
 })()
